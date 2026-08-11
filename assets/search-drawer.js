a8d0e12cedbb92417fa3b29b8155ada49fa39a13
Cass Harper <cass@beae.com>
2026-08-11 16:01:53 +0700

    fix: slow search placeholder rotation

--- assets/search-drawer.js
+++ assets/search-drawer.js
@@ -669,8 +669,8 @@ import { A11y, Swiper } from './swiper-loader.js';
           this.placeholderIndex = (this.placeholderIndex + 1) % this.placeholderTerms.length;
           this.animatedPlaceholder.textContent = this.placeholderTerms[this.placeholderIndex];
           this.animatedPlaceholder.classList.remove('is-changing');
-        }, 160);
-      }, 2800);
+        }, 220);
+      }, 4500);
     }
 
     stopPlaceholderAnimation() {
